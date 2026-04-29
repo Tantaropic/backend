294e59dee8ecbd9397fd056ade34ebbfd02b7b7f
MohamedMansour404 <mohamedmansour.personal1@gmail.com>
2026-04-29 18:19:57 +0300

    fix: register mock modules in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../database/prisma/prisma.module';
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from '../modules/users/users.module';
+import { MockBankModule } from '../modules/mock-bank/mock-bank.module';
+import { MockExchangeModule } from '../modules/mock-exchange/mock-exchange.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
+    MockBankModule,
+    MockExchangeModule,
   ],
   controllers: [AppController],
   providers: [],
